9306659107531a398aa9ee6e998404571f539243
Marshal10 <marshalmaleane123gmail.com>
2023-12-27 15:08:39 +0530

    Generate random cars

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,26 @@
+from turtle import Turtle
+from random import choice,randint
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.create_car()
+        
+        
+    def create_car(self):
+        random_y=randint(-250,250)
+        random_x=randint(280,300)
+        self.shape("square")
+        self.penup()
+        self.shapesize(stretch_wid=1, stretch_len=2)
+        self.color(choice(COLORS))
+        self.setheading(180)
+        self.goto(random_x,random_y)
+        
+        
+    def move(self):
+        self.forward(STARTING_MOVE_DISTANCE)
--- main.py
+++ main.py
@@ -3,18 +3,27 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
-
+cars=[]
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.listen()
 
 player=Player()
+index=0
 screen.onkeypress(player.move,"Up")
-
+for i in range(25):
+    if i%6==0:
+        cars.append(CarManager())
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    if index%6==0:
+        cars.append(CarManager())  
+    index+=1    
+    for car in cars:
+        car.move()
+        
 
 screen.exitonclick()
